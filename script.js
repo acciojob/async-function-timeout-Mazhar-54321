c2453b8922055546aec098cb51a63debb3764db3
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-12 08:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+async function clickHandler(){
+	let text = document.getElementById("text").value;
+	let delay = document.getElementById("delay").value;
+	function delayText(){
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				resolve(text)
+			},delay)
+		})
+	}
+	let text1 = await delayText();
+	document.getElementById("output").textContent=text1;
+}
